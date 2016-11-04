f9d831fe1bbcc93a433f4641cc4f500a89bfc21d
Pablo Pazos <pablo.pazos@cabolabs.com>
2016-11-04 00:00:22 -0300

    added patient and author data to CDA header

--- cda_builder.groovy
+++ cda_builder.groovy
@@ -26,11 +26,12 @@ line 2:
  1 ClinicalDocument.author.assignedAuthor.assignedPerson.given
  2 ClinicalDocument.author.assignedAuthor.assignedPerson.given
  3 ClinicalDocument.author.assignedAuthor.assignedPerson.family
- 4 ClinicalDocument.author.assignedAuthor.id.extension
- 5 ClinicalDocument.author.assignedAuthor.id.root
- 6 ClinicalDocument.author.assignedAuthor.representedOrganization.id.extension
- 7 ClinicalDocument.author.assignedAuthor.representedOrganization.id.root
- 8 ClinicalDocument.author.assignedAuthor.representedOrganization.name
+ 4 ClinicalDocument.author.assignedAuthor.assignedPerson.family
+ 5 ClinicalDocument.author.assignedAuthor.id.extension
+ 6 ClinicalDocument.author.assignedAuthor.id.root
+ 7 ClinicalDocument.author.assignedAuthor.representedOrganization.id.extension
+ 8 ClinicalDocument.author.assignedAuthor.representedOrganization.id.root
+ 9 ClinicalDocument.author.assignedAuthor.representedOrganization.name
 
 line 3:
  0 ClinicalDocument.custodian
@@ -52,7 +53,7 @@ Date.metaClass.static.nowString = { ->
 
 def header_file = new File(args[0])
 def header_data_csv = header_file.text
-def header_data = header_data_csv.normalize().tokenize("\n")*.tokenize(",;") //*.trim()
+def header_data = header_data_csv.normalize().tokenize("\n")*.split(",") //*.trim()
 
 println header_data
 
@@ -61,6 +62,8 @@ println header_data
 def writer = new StringWriter()
 def root = new MarkupBuilder(writer)
 root.setDoubleQuotes(true)
+
+// Build CDA Header
 
 root.ClinicalDocument( xmlns:      'urn:hl7-org:v3',
                       'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
@@ -75,6 +78,55 @@ root.ClinicalDocument( xmlns:      'urn:hl7-org:v3',
    languageCode(code: header_data[0][4])
    setId(root: String.uuid())
    versionNumber(value: 1)
+   
+   // Patient
+   recordTarget {
+      patientRole {
+         id(root: header_data[1][7], extension: header_data[1][6])
+         patient {
+            name {
+               given(header_data[1][0])
+               family(header_data[1][2])
+            }
+            administrativeGenderCode(code: header_data[1][5], codeSystem: '2.16.840.1.113883.5.1')
+            birthTime(value: header_data[1][4])
+         }
+      }
+   }
+   /*
+   // Doctor
+   author {
+      time(Date.nowString())
+      assignedAuthor {
+         id(root: header_data[][], extension: header_data[][])
+         assignedPerson {
+            name {
+               prefix(header_data[][])
+               given(header_data[][])
+               family(header_data[][])
+            }
+         }
+         representedOrganization {
+            id(root: header_data[][], extension: header_data[][])
+            name(header_data[][])
+         }
+      }
+   }
+   
+   // Organization
+   custodian {
+      assignedCustodian {
+         representedCustodianOrganization {
+            id(root: header_data[][])
+            name(header_data[][])
+            // TODO: TEL, ADDR
+         }
+      }
+   }
+   
+   // Body
+   // TODO
+   */
 }
 
 def cda = writer.toString()
