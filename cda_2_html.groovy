d0f8c8b17d06029bfd782745561ed4477ab0a254
Pablo Pazos <pablo.pazos@cabolabs.com>
2017-08-17 19:43:40 -0300

    removed reference to local bootstrap, added CDN

--- cda_2_html.groovy
+++ cda_2_html.groovy
@@ -23,8 +23,8 @@ html.html(lang:'es') {
     meta  ('http-equiv': "X-UA-Compatible", content: "IE=edge")
     meta  (name: "viewport", content: "width=device-width, initial-scale=1")
     title (cda.title.text())
+    link  (rel: "stylesheet", href: "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css", integrity: "sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u", crossorigin: "anonymous")
     link  (href: "http://fonts.googleapis.com/css?family=Roboto:100,300,400,700", rel: "stylesheet")
-    link  (href: "bootstrap/css/bootstrap.min.css", rel:"stylesheet")
   }
   body() {
     div(class: 'container') {
