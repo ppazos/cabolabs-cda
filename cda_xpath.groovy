c0dab6b0a4b696ed4f81d35cbbc9af6eda68673e
Pablo Pazos <pablo.pazos@cabolabs.com>
2017-08-17 23:43:27 -0300

    Improvements on results for CDA queries by XPath

--- cda_xpath.groovy
+++ cda_xpath.groovy
@@ -7,6 +7,7 @@ groovy cda_xpath.groovy cda.xml
 
 if (args.size() == 0)
 {
+   println "\n"
    println usage
    return 0
 }
@@ -18,15 +19,30 @@ def processXml( String xml, String xpathQuery )
   def inputStream = new ByteArrayInputStream( xml.bytes )
   def records     = builder.parse(inputStream).documentElement
   def nodes = xpath.evaluate( xpathQuery, records, XPathConstants.NODESET )
-  def res = []
+  def res = [:]
+  def name
   nodes.each { node ->
-    if (node.textContent.trim()) res << node.textContent
+
+    // text nodes have name #text
+    name = node.nodeName
+    if (name == '#text') name = node.parentNode.nodeName
+    
+    // only add results with data
+    if (node.textContent.trim())
+    {
+      if (res[name] == null) res[name] = []
+      res[name] << node.textContent
+    }
+    //if (node.textContent.trim()) res << node.textContent
+    //if (node.textContent.trim()) res[node.nodeName] = node.textContent
   }
   return res
 }
 
 def xml = new File(args[0])
 def xml_string = xml.text
+
+println ""
 
 def atributos_documento = processXml( xml_string, '/ClinicalDocument/*/text()' )
 println "atributos_documento 1: "+ atributos_documento
@@ -46,4 +62,4 @@ println "id_paciente: "+ id_paciente
 def organizacion = processXml( xml_string, '/ClinicalDocument/custodian//text()' )
 println "organizacion: "+ organizacion
 
-
+println ""
