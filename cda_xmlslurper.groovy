dc0c2faf8b6385a4fa969e783df9babf8a419916
Pablo Pazos <pablo.pazos@cabolabs.com>
2017-08-17 23:43:42 -0300

    output styling

--- cda_xmlslurper.groovy
+++ cda_xmlslurper.groovy
@@ -6,6 +6,7 @@ groovy cda_xmlslurper.groovy cda.xml
 
 if (args.size() == 0)
 {
+   println "\n"
    println usage
    return 0
 }
@@ -16,6 +17,8 @@ def clinical_doc = new XmlSlurper().parseText(xml_string)
 
 //println clinical_doc*.text()
 
+println ""
+
 def nombre_autor = clinical_doc.author.assignedAuthor.assignedPerson.name.children().collect{ it.text() }
 println "nombre_autor: "+ nombre_autor
 
@@ -24,3 +27,5 @@ println "nombre_paciente: "+ nombre_paciente
 
 def id_paciente = clinical_doc.recordTarget.patientRole.id[0].attributes()
 println "id_paciente: "+ id_paciente
+
+println ""
